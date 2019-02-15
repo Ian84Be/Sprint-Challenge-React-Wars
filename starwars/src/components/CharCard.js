b8cc5fb7e7840024bd434bf32d1c6533eca11766
Ian Be <ian84be@gmail.com>
2019-02-15 12:37:46 -0600

    tweaks

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 const CharCard = (props) => {
-    return ( 
+    return (
         <div className="char-card-container">
-        {props.charList.map(item => {
-            console.log(item.name);
-            return (
-                <div className={`char-card ${item.name}`} key={item.name}>
-                <h2>{item.name}</h2>
-                <p>BORN: {item.birth_year}</p>
-                </div>
-            );
-        })}
+
+            {props.charList.map(item => {
+
+                let homeworldName = '';
+
+                fetch(item.homeworld)
+                    .then(res => {
+                        return res.json();
+                    })
+                    .then(data => {
+                        homeworldName = data.name;
+                        console.log(homeworldName);
+                    })
+                    .catch(err => {
+                        throw new Error(err);
+                    });
+                
+                return (
+                    <div className={`char-card ${item.name}`} key={item.name}>
+                        <h2>{item.name}</h2>
+                        <i className="far fa-eye fa-3x" style={{ color: item.eye_color }}></i>
+                        <i className="far fa-eye fa-3x" style={{ color: item.eye_color }}></i>
+                        <span>&nbsp;</span>
+                        <p>BORN: {item.birth_year}</p>
+                        <p>HOMEWORLD: {homeworldName}</p>
+                    </div>
+                );
+            })}
         </div>
-     )
-}
- 
+    );
+};
+
 export default CharCard;
